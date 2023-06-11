15762c51f6434d4d7f6675169c5e71dffe03db97
AngelMauricioRamirez <A01710158@tec.mx>
2023-06-10 23:32:59 -0600

    Se cambio el vector materia

--- headers/Curso.h
+++ headers/Curso.h
@@ -23,7 +23,7 @@ class Curso {
 private:
   // Variables iniciales
   string nombre;
-  vector<Materia *> materias;
+  vector<Materia> materias;
 
 public:
   // Constructor
@@ -34,7 +34,7 @@ public:
 
   // Getters
   string get_nombre();
-  Materia *get_materia(string);
+  Materia get_materia(string);
   // Setters
   void set_nombre(string);
   void set_materia(string, int, string, double, string, int, int);
@@ -53,8 +53,7 @@ Curso::Curso(string nombre, string nombreMat, int nivelMat, string profesorMat,
              double calificacionMat, string edificio, int numero,
              int capacidad) {
   this->nombre = nombre;
-  Materia *materiaInicial =
-      new Materia(nombreMat, nivelMat, profesorMat, calificacionMat, edificio,
+  Materia materiaInicial(nombreMat, nivelMat, profesorMat, calificacionMat, edificio,
                   numero, capacidad);
   materias.push_back(materiaInicial);
 }
@@ -80,13 +79,13 @@ string Curso::get_nombre() { return nombre; }
  * @return nullptr
  */
 
-Materia *Curso::get_materia(string nombre) {
-  for (Materia *materia : materias) {
-    if (materia->get_nombre() == nombre) {
+Materia Curso::get_materia(string nombre) {
+  for (Materia materia : materias) {
+    if (materia.get_nombre() == nombre) {
       return materia;
     }
   }
-  return nullptr; // La materia no se encontró
+  return Materia(); // La materia no se encontró
 }
 
 // Setters
@@ -113,7 +112,7 @@ void Curso::set_nombre(string nombre) { this->nombre = nombre; }
 void Curso::set_materia(string nombre, int nivel, string profesor,
                         double calificacion, string edificio, int numero,
                         int capacidad) {
-  Materia *materia = new Materia(nombre, nivel, profesor, calificacion,
+  Materia materia(nombre, nivel, profesor, calificacion,
                                  edificio, numero, capacidad);
   materias.push_back(materia);
 }
@@ -130,11 +129,13 @@ void Curso::set_materia(string nombre, int nivel, string profesor,
 
 void Curso::show_materias() {
   // Se imprimen todas las materias que el objeto tiene registradas
-  for (Materia *materia : materias) {
-    cout << "- " << materia->get_nombre() << " nivel: " << materia->get_nivel()
-         << " calificacion: " << materia->get_calificacion()
-         << " profesor: " << materia->get_profesor() << "\n";
-    cout << "- " << materia->get_aula().get_edificio() << "\n";
+  for (Materia materia : materias) {
+    cout << "Nombre de la materia: " << materia.get_nombre() << "\n";
+    cout << "Nivel: " << materia.get_nivel() << "\n";
+    cout << "Calificacion: " << materia.get_calificacion() << "\n";
+    cout << "Profesor: " << materia.get_profesor() << "\n";
+    cout << "Edificio: " << materia.get_aula().get_edificio() << "\n";
+    cout << "Salon: " << materia.get_aula().get_numero() << "\n";
   }
 }
 
@@ -150,8 +151,7 @@ void Curso::show_materias() {
 void Curso::remove_materia(string nombre){
 //  Elimina la materia buscada por el nombre
   for(int i = 0; i < materias.size(); i++){
-    if(materias[i]->get_nombre() == nombre){
-      delete materias[i];
+    if(materias[i].get_nombre() == nombre){
       materias.erase(materias.begin() + i);
       break; // Termina el bucle después de encontrar y eliminar la materia
     }
